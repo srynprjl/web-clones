12fe54d0d25e8eee465bf62d8e32af4c7bfaf88e
Shreyan Parajuli <account@sysnefo.com>
2026-07-30 23:55:13 +0545

    [html/spotify] add playlist elements

--- html/spotify/assets/js/script.js
+++ html/spotify/assets/js/script.js
@@ -71,6 +71,12 @@ playlists.forEach((data) => {
 
 })
 
+let defaultPlaylist = "play1"
+
+let playlistHeader = document.getElementById("playlist-header-item");
+
+
+
 let playbar = document.getElementById("bar");
 playbar.addEventListener("input", (e) => {
   playbar.style.setProperty('--value', `${e.target.value}%`);
@@ -99,14 +105,372 @@ volume.addEventListener("input", updateVolume)
 volume.addEventListener("change", updateVolume)
 
 
-const songs =
+const tempSongs  = [
+    {
+        "id": "D6snwsqL64s",
+        "title": "Donald Trumpko Bhai",
+        "thumbnail": "https://i.ytimg.com/vi/D6snwsqL64s/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBTa8Gv0grDBlQtjfjwxaEr_wwpHA",
+        "url": "https://music.youtube.com/watch?v=D6snwsqL64s",
+        "time": null,
+        "artist": "Rajkumar Oli - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "D6snwsqL64s.mp3"
+    },
+    {
+        "id": "iXTo1jzUbYk",
+        "title": "Tanatan Hanahan Syauli Bajarma",
+        "thumbnail": "https://i.ytimg.com/vi/iXTo1jzUbYk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAvFA_t5URHqsqJ_BmGBghhVfGmSg",
+        "url": "https://music.youtube.com/watch?v=iXTo1jzUbYk",
+        "time": null,
+        "artist": "Bishal Sony",
+        "albums": null,
+        "date": null,
+        "local_path": "iXTo1jzUbYk.mp3"
+    },
+    {
+        "id": "X67Vewz7908",
+        "title": "Sailo Ma Charghare",
+        "thumbnail": "https://i.ytimg.com/vi/X67Vewz7908/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDdhoIVMLh_KsEUaZR6MCfxYrA16Q",
+        "url": "https://music.youtube.com/watch?v=X67Vewz7908",
+        "time": null,
+        "artist": "Bishwo Dong ",
+        "albums": null,
+        "date": null,
+        "local_path": "X67Vewz7908.mp3"
+    },
+    {
+        "id": "5h8e6LLK9Ho",
+        "title": "Kasmire Pachhyauri",
+        "thumbnail": "https://i.ytimg.com/vi/5h8e6LLK9Ho/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4ksO5UdRyDWOh3GA4lDC7ECG54Q",
+        "url": "https://music.youtube.com/watch?v=5h8e6LLK9Ho",
+        "time": null,
+        "artist": "Nischal Dawadi - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "5h8e6LLK9Ho.mp3"
+    },
+    {
+        "id": "qYIa6_AhEeI",
+        "title": "Gorkha Paltan",
+        "thumbnail": "https://i.ytimg.com/vi/qYIa6_AhEeI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDHHm3FprW1rYUVF2VKeh7k1jI7nw",
+        "url": "https://music.youtube.com/watch?v=qYIa6_AhEeI",
+        "time": null,
+        "artist": "Narayan Rayamajhi",
+        "albums": null,
+        "date": null,
+        "local_path": "qYIa6_AhEeI.mp3"
+    },
+    {
+        "id": "5kl1i4yOGM8",
+        "title": "Ma Ta Dhale Dhale",
+        "thumbnail": "https://i.ytimg.com/vi/5kl1i4yOGM8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB4YW0cg2tSmbwM2EfrkvcXmSmyrQ",
+        "url": "https://music.youtube.com/watch?v=5kl1i4yOGM8",
+        "time": null,
+        "artist": "Shanta Rani Pariyar - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "5kl1i4yOGM8.mp3"
+    },
+    {
+        "id": "ttW2d4jEHP0",
+        "title": "Rukum Maikot",
+        "thumbnail": "https://i.ytimg.com/vi/ttW2d4jEHP0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBD1jXaqSY2-d2SsOZwWMfFlcbxPg",
+        "url": "https://music.youtube.com/watch?v=ttW2d4jEHP0",
+        "time": null,
+        "artist": "Ashok Thapa Magar - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "ttW2d4jEHP0.mp3"
+    },
+    {
+        "id": "erObjmedbNQ",
+        "title": "Namaste Dhogdiya",
+        "thumbnail": "https://i.ytimg.com/vi/erObjmedbNQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBiQOrUd-J68vil32INst1wCt0chQ",
+        "url": "https://music.youtube.com/watch?v=erObjmedbNQ",
+        "time": null,
+        "artist": "Kabita khatri - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "erObjmedbNQ.mp3"
+    },
+    {
+        "id": "f8f46PMowzU",
+        "title": "UK Basne Maya",
+        "thumbnail": "https://i.ytimg.com/vi/f8f46PMowzU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBBdsRsVFat9Z_ybrD-9NuvJfjkhg",
+        "url": "https://music.youtube.com/watch?v=f8f46PMowzU",
+        "time": null,
+        "artist": "Badri Sapkota - Topic",
+        "albums": null,
+        "date": null,
+        "local_path": "f8f46PMowzU.mp3"
+    },
+    {
+        "id": "GvXin8l9Tjg",
+        "title": "Bandipuraima",
+        "thumbnail": "https://i.ytimg.com/vi/GvXin8l9Tjg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAIbe3-RLVcrPMKmcvIlpES8xQeaQ",
+        "url": "https://music.youtube.com/watch?v=GvXin8l9Tjg",
+        "time": null,
+        "artist": "Prem Raja Mahat",
+        "albums": null,
+        "date": null,
+        "local_path": "GvXin8l9Tjg.mp3"
+    },
+    {
+        "id": "KAjx1y3uc6c",
+        "title": "Kafle",
+        "thumbnail": "https://i.ytimg.com/vi/KAjx1y3uc6c/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAwZwK6u7E_iuvcgNO52vwbw41iMA",
+        "url": "https://music.youtube.com/watch?v=KAjx1y3uc6c",
+        "time": null,
+        "artist": "Jhuma Limbu",
+        "albums": null,
+        "date": null,
+        "local_path": "KAjx1y3uc6c.mp3"
+    },
+    {
+        "id": "H2xaiUjYbRw",
+        "title": "Bardali",
+        "thumbnail": "https://i.ytimg.com/vi/H2xaiUjYbRw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB1w85ncS76F6gmU1H-lxjLRgd6pQ",
+        "url": "https://music.youtube.com/watch?v=H2xaiUjYbRw",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "H2xaiUjYbRw.mp3"
+    },
+    {
+        "id": "WIo7c7L8dOA",
+        "title": "Aunty Lover",
+        "thumbnail": "https://i.ytimg.com/vi/WIo7c7L8dOA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDxfGpnY7X6DHJXbCg9Azbj3A-HTQ",
+        "url": "https://music.youtube.com/watch?v=WIo7c7L8dOA",
+        "time": null,
+        "artist": "Kala Lamsal",
+        "albums": null,
+        "date": null,
+        "local_path": "WIo7c7L8dOA.mp3"
+    },
+    {
+        "id": "7oyyAUhbJ3s",
+        "title": "Kal Dhara",
+        "thumbnail": "https://i.ytimg.com/vi/7oyyAUhbJ3s/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDAz2-GnmiF-jD6x4L11UDpNiYtBQ",
+        "url": "https://music.youtube.com/watch?v=7oyyAUhbJ3s",
+        "time": null,
+        "artist": "Mr. D",
+        "albums": null,
+        "date": null,
+        "local_path": "7oyyAUhbJ3s.mp3"
+    },
+    {
+        "id": "gRsUsITcJ40",
+        "title": "Parkha Na",
+        "thumbnail": "https://i.ytimg.com/vi/gRsUsITcJ40/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLByn-k4numBQwj_0sLB03IpYfPK9g",
+        "url": "https://music.youtube.com/watch?v=gRsUsITcJ40",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "gRsUsITcJ40.mp3"
+    },
+    {
+        "id": "iypUH0Cf-hs",
+        "title": "Sushant KC - Tabahi ft. Asmick Khawas & Yashaswi Khadka (Official\u00a0Music\u00a0Video)",
+        "thumbnail": "https://i.ytimg.com/vi/iypUH0Cf-hs/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBBY4RsECENp5ojAh5q4M49VxXcxA",
+        "url": "https://music.youtube.com/watch?v=iypUH0Cf-hs",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "iypUH0Cf-hs.mp3"
+    },
+    {
+        "id": "9ZsboG8vH_U",
+        "title": "Pahuna",
+        "thumbnail": "https://i.ytimg.com/vi/9ZsboG8vH_U/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDWWJmNfLzI82NS0kiOsewLJlyuxQ",
+        "url": "https://music.youtube.com/watch?v=9ZsboG8vH_U",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "9ZsboG8vH_U.mp3"
+    },
+    {
+        "id": "uLpQcLaAPzM",
+        "title": "Ji Chanta Matina",
+        "thumbnail": "https://i.ytimg.com/vi/uLpQcLaAPzM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB0nWC-EDNmBuC-O1_c5BCuFMvkxw",
+        "url": "https://music.youtube.com/watch?v=uLpQcLaAPzM",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "uLpQcLaAPzM.mp3"
+    },
+    {
+        "id": "8iAmuzmlvhY",
+        "title": "Pagal",
+        "thumbnail": "https://i.ytimg.com/vi/8iAmuzmlvhY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBQzNaKhcPKuBAlTBAjOEoQkXzjNw",
+        "url": "https://music.youtube.com/watch?v=8iAmuzmlvhY",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "8iAmuzmlvhY.mp3"
+    },
+    {
+        "id": "U5tzA0Q58bw",
+        "title": "I Like You (Anischaya)",
+        "thumbnail": "https://i.ytimg.com/vi/U5tzA0Q58bw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDANEpPtVEIF1nb1Op-ZNNUkv7kig",
+        "url": "https://music.youtube.com/watch?v=U5tzA0Q58bw",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "U5tzA0Q58bw.mp3"
+    },
+    {
+        "id": "B05rPYmiePg",
+        "title": "Phone Ko No. ? (feat. Foeseal)",
+        "thumbnail": "https://i.ytimg.com/vi/B05rPYmiePg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDG75ZpGsbjNMoBQH_XqOv6jEIIhQ",
+        "url": "https://music.youtube.com/watch?v=B05rPYmiePg",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "B05rPYmiePg.mp3"
+    },
+    {
+        "id": "uH3qChuA-MY",
+        "title": "Baby",
+        "thumbnail": "https://i.ytimg.com/vi/uH3qChuA-MY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBRXLSeTmofZ9nsH-EjFkhWq0wzIQ",
+        "url": "https://music.youtube.com/watch?v=uH3qChuA-MY",
+        "time": null,
+        "artist": "Ujan Shakya",
+        "albums": null,
+        "date": null,
+        "local_path": "uH3qChuA-MY.mp3"
+    },
+    {
+        "id": "qOAe3fOAC_s",
+        "title": "Ma Tira Farki Herana",
+        "thumbnail": "https://i.ytimg.com/vi/qOAe3fOAC_s/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDr1rzuPCCxy_GEh5upaShcaNphYQ",
+        "url": "https://music.youtube.com/watch?v=qOAe3fOAC_s",
+        "time": null,
+        "artist": "Karan Bhatta",
+        "albums": null,
+        "date": null,
+        "local_path": "qOAe3fOAC_s.mp3"
+    },
+    {
+        "id": "cfPqb-qLXdE",
+        "title": "Rangin",
+        "thumbnail": "https://i.ytimg.com/vi/cfPqb-qLXdE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD8UNY2lVnfoph23Un9SJFmsp7Idw",
+        "url": "https://music.youtube.com/watch?v=cfPqb-qLXdE",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "cfPqb-qLXdE.mp3"
+    },
+    {
+        "id": "Qili95kUNQk",
+        "title": "Timi Nacha Na",
+        "thumbnail": "https://i.ytimg.com/vi/Qili95kUNQk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD9Pa0gyPw3AgtpNAlRsPjNyBUSpw",
+        "url": "https://music.youtube.com/watch?v=Qili95kUNQk",
+        "time": null,
+        "artist": "Wangden Sherpa",
+        "albums": null,
+        "date": null,
+        "local_path": "Qili95kUNQk.mp3"
+    },
+    {
+        "id": "1BvLlwDOsSQ",
+        "title": "Akkha Cha",
+        "thumbnail": "https://i.ytimg.com/vi/1BvLlwDOsSQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZmPWxIVjoRSMmO_GbTxfXHx-CVQ",
+        "url": "https://music.youtube.com/watch?v=1BvLlwDOsSQ",
+        "time": null,
+        "artist": "Mr. D",
+        "albums": null,
+        "date": null,
+        "local_path": "1BvLlwDOsSQ.mp3"
+    },
+    {
+        "id": "zQYGDpGdxTU",
+        "title": "Taato Goli (feat. Melina Rai)",
+        "thumbnail": "https://i.ytimg.com/vi/zQYGDpGdxTU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAUlLSswnnoyJnPPgXzy029ugvDGg",
+        "url": "https://music.youtube.com/watch?v=zQYGDpGdxTU",
+        "time": null,
+        "artist": "\ud30c\uc6cc\ubcfc \uace0\ub77c\ub2c8",
+        "albums": null,
+        "date": null,
+        "local_path": "zQYGDpGdxTU.mp3"
+    },
+    {
+        "id": "SX_AdP5XC8A",
+        "title": "Jhim Jhimaune Aankha",
+        "thumbnail": "https://i.ytimg.com/vi/SX_AdP5XC8A/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgBtgiAArgIigIMCAAQARh_IEQoFjAP&rs=AOn4CLCUEVXP9B-mYfqIxmHcpS_2K5FM2Q",
+        "url": "https://music.youtube.com/watch?v=SX_AdP5XC8A",
+        "time": null,
+        "artist": "Ekdev Limbu",
+        "albums": null,
+        "date": null,
+        "local_path": "SX_AdP5XC8A.mp3"
+    },
+    {
+        "id": "Dt1P5I0koXY",
+        "title": "Fakauna Ma",
+        "thumbnail": "https://i.ytimg.com/vi/Dt1P5I0koXY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBQLuGhKayvoyCYM3QlgB4DZnQAow",
+        "url": "https://music.youtube.com/watch?v=Dt1P5I0koXY",
+        "time": null,
+        "artist": "Sushant KC",
+        "albums": null,
+        "date": null,
+        "local_path": "Dt1P5I0koXY.mp3"
+    },
+    {
+        "id": "kbwuCugRFB0",
+        "title": "Shaky",
+        "thumbnail": "https://i.ytimg.com/vi/kbwuCugRFB0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAS1te8V1pZn6KqRP1F8m41qbPG4w",
+        "url": "https://music.youtube.com/watch?v=kbwuCugRFB0",
+        "time": null,
+        "artist": "Sanju Rathod SR",
+        "albums": null,
+        "date": null,
+        "local_path": "kbwuCugRFB0.mp3"
+    }
+]
+
+let songContainer = document.getElementById('playlist-songs')
+let index=1
+tempSongs.forEach(song => {
+  const songsHTMLTemplate =
+    `
+    <div class="song-container" id="${song.id}">
+        <div class="song-index">${index}</div>
+        <div style="display:flex;" class="song-title">
+          <div><img class="song-img" src="${song.thumbnail}" /></div>
+          <div>
+            <div class="song-title">${song.title}</div>
+            <div class="song-artist">${song.artist}</div>
+          </div>
+        </div>
+        <div>${song.albums}</div>
+        <div>${song.date}</div>
+        <div>${song.time}</div>
+    </div>
+    `
+  index++;
+  songContainer.innerHTML += songsHTMLTemplate
+})
+
+// let audioElement = new Audio(`assets/songs/${tempSongs[0].local_path}`)
 
 // files
 function prevSong(){}
 
 function nextSong(){}
 
-function playPause(){}
+function playPause(id) {
+  // audioElement.play()
+}
 
 function repeat(){}
 
